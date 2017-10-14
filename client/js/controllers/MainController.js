871b9160ad38ea23faf6834a8f1bf9f5fdec2f24
ashuels <ashuels@siue.edu>
2017-10-14 08:27:49 -0500

    Trying to fix Search
    Working on search filter

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -1,5 +1,23 @@
-app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {
+app.controller('MainController', ['$scope', 'forecast', function ($scope, forecast, SearchService) {
   forecast.success(function(data) {
     $scope.fiveDay = data;
   });
+    $scope.searchVars = SearchService.searchVars;
+    $scope.newVar = {
+        val: ""
+    };
+    SearchService.newVal = $scope.newVar;
+
 }]);
+
+
+app.controller('SearchController', function($scope, searchForm, SearchService) {
+    $scope.searchVars = SearchService.searchVars; 
+    $scope.anotherVar = SearchService.newVar;
+});
+
+app.service("SearchService", function() {
+    return {
+        searchVars: [{name: "dummy"}, {name: "dummy"}]
+    }
+});
---
+++ client/js/controllers/SearchController.js
@@ -0,0 +1,6 @@
+app.controller('SearchController', function($scope) {
+    $scope.lastnamesearch = "";
+    $scope.firstnamesearch = "";
+    $scope.statesearch = "";
+    $scope.breedsearch = "";
+});
