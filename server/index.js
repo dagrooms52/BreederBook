cf25745133989a5c487ce00b7075e3570d686f0a
ashuels <ashuels@siue.edu>
2017-10-14 13:37:59 -0500

    Now they actually connect

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ var setup = new Setup(server);
 server.register({
     register: Cors,
     options: {
-        origins: ['http://127.0.0.1:63991']
+        origins: ['http://127.0.0.1:54680']
     }
 }, function(err){
     server.start(function(err){
