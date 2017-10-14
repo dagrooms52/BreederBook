bedd59537a975093b228f6daad01e204f8af042c
ashuels <ashuels@siue.edu>
2017-10-14 12:33:38 -0500

    Added Rating Form
    Rate Breeder implemented, still not connected to live data

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -1,15 +1,27 @@
-'use strict';
+//app.controller('MainController', ['$scope', 'forecast', function ($scope, forecast, SearchService) {
+//  forecast.success(function(data) {
+//    $scope.fiveDay = data;
+//  });
+//    $scope.searchVars = SearchService.searchVars;
+//    $scope.newVar = {
+//        val: ""
+//    };
+//    SearchService.newVal = $scope.newVar;
+//
+//}]);
+// declare the app with no dependencies
+var myApp = angular.module('myApp', []);
 
-var app = angular.module('myApp', []);
+// Create the factory that share the Fact
+myApp.factory('Fact', function(){
+  return { Field: '' };
+});
 
-app.controller('MainController', function ($scope, $http) {
-    $http.get("http://localhost:3000/breeders/BJbW8c16b")
-    .then(function(response) {
-        //First function handles success
-        $scope.content = response.data;
-    }, function(err) {
-        //Second function handles error
-        $scope.content = err;
-    });
-    
+// Two controllers sharing an object that has a string in it
+myApp.controller('FirstCtrl', function( $scope, Fact ){
+  $scope.Alpha = Fact;
+});
+
+myApp.controller('SecondCtrl', function( $scope, Fact ){
+  $scope.Beta = Fact;
 });
