b81ba971696ed72caa155925ace855ac0b05f3e6
ashuels <ashuels@siue.edu>
2017-10-15 09:38:26 -0500

    Decent website

--- client/js/controllers/SearchController.js
+++ client/js/controllers/SearchController.js
@@ -1,6 +1,25 @@
-app.controller('SearchController', function($scope) {
-    $scope.lastnamesearch = "";
-    $scope.firstnamesearch = "";
-    $scope.statesearch = "";
-    $scope.breedsearch = "";
-});
+var app = angular.module("myApp", []);
+    app.controller("HttpFilterController", function ($scope, $http) {
+
+        $scope.SearchData = function () {
+
+            var config = {
+              params: {
+//                "name.first": $scope.firstname,
+//                "name.last": $scope.lastname,
+                "city": $scope.city,
+                "state": $scope.state
+              }
+            };
+            
+
+            $http.get('https://petsource-pethacks.herokuapp.com/breeders', config)
+            .success(function (response) {
+                $scope.PostDataResponse = response;
+            })
+            .error(function (response) {
+                $scope.ResponseDetails = response
+            });
+        };
+
+    });
